ab0ed119e37511d99d84a39bf55caefdde0cfbf2
Miguel Sarenas <migui0323@gmail.com>
2023-01-13 21:57:29 -0500

    API now responds to custom prompts entered by users

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 require("dotenv").config();
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
@@ -15,6 +14,8 @@ export default async function handler(
 ) {
   // TODO: make sure to clean the data before passing it into the model and make sure its valid according to the parameters of the create completion api function
   const prompt = req.body.prompt;
+  console.log(prompt);
+
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
@@ -22,12 +23,13 @@ export default async function handler(
       //   'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:',
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 100,
+      max_tokens: 3000,
       top_p: 1,
-      frequency_penalty: 0.0,
+      frequency_penalty: 0.5,
       presence_penalty: 0.0,
-      stop: ["\n"],
     });
+    // TODO: figure out why there isnt any data in the response from the model
+
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
