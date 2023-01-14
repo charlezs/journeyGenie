6ef821b46724e53a64999db4f42a31c3d1bf6105
Miguel Sarenas <migui0323@gmail.com>
2023-01-13 21:36:30 -0500

    some refactoring

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,12 +9,18 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const generateText = async () => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // TODO: make sure to clean the data before passing it into the model and make sure its valid according to the parameters of the create completion api function
+  const prompt = req.body.prompt;
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:',
+      // prompt:
+      //   'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:',
+      prompt: `${prompt}`,
       temperature: 0,
       max_tokens: 100,
       top_p: 1,
@@ -22,19 +28,13 @@ const generateText = async () => {
       presence_penalty: 0.0,
       stop: ["\n"],
     });
-    return response.data.choices[0];
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).send({ error: error });
   }
-};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
   // TODO: be careful about parsing the prompt text to make sure its inputted into the model correctly
-  const generatedText = await generateText();
-  // console.log(generatedText);
-
-  res.status(200).json({ generatedText });
 }
